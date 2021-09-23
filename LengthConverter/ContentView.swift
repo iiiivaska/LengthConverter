0ea7df91aa103b10a78e706b3834f44372e25b30
iiiivaska <iiiivaska@yandex.ru>
2021-09-23 16:57:47 +0300

    Initial Commit

---
+++ LengthConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LengthConverter
+//
+//  Created by Василий Буланов on 9/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Василий Буланов on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
